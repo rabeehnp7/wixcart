0d87befb616e4311fd4098608402f06727b34646
Rabeeh <rabeeh.peridot@gmail.com>
2025-07-03 01:59:36 +0530

    clear some unwanted codes

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='home'),
     path('product_list',views.product_list,name='product_list'),
-    path('product_detail/<int:pro_id>/',views.product_detail,name='product_detail'),
-    path('run',views.run_collectstatic,name='run'),
+    path('product_detail/<int:pro_id>/',views.product_detail,name='product_detail')
 ]
